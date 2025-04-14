d8eec27576e012e6e08365baa3db415cb83014f9
RJ-MONIKA <monikarj.22aid@kongu.edu>
2025-04-14 23:36:12 +0530

    frontend updated

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -79,14 +79,7 @@ function Login() {
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Signin to your account
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or
-              <span
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                start your 14-day free trial
-              </span>
-            </p>
+            
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             {/* <input type="hidden" name="remember" defaultValue="true" /> */}
