3d423fa77c3d399114e2280ad9fa6e33c7105e51
RJ-MONIKA <monikarj.22aid@kongu.edu>
2025-05-22 20:52:39 +0530

    frontend added in ml model

--- ML model/app.py
+++ ML model/app.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://inventory-management-4.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
