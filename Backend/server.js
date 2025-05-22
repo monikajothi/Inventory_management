c9cd729d486c9625d7ac50472ad927a0c2a9ae3e
RJ-MONIKA <monikarj.22aid@kongu.edu>
2025-05-22 09:32:08 +0530

    frontend deployed

--- Backend/server.js
+++ Backend/server.js
@@ -16,10 +16,11 @@ main();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://inventory-management-4.onrender.com"],
     credentials: true,
   })
 );
+
 
 // Store API
 app.use("/api/store", storeRoute);
