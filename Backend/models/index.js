dab1a7d715a05a9c66c0b3400db7ed5720ffd786
monikajothi <monikajothi07@gmail.com>
2026-04-26 13:57:13 +0530

    ss added, uri changed to nonsrv

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://monikajothi07:HJXUxZZnEwXGtJbT@cluster0.2sqz0.mongodb.net/inventory-management?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb://monikajothi07:HJXUxZZnEwXGtJbT@cluster0-shard-00-00.2sqz0.mongodb.net:27017,cluster0-shard-00-01.2sqz0.mongodb.net:27017,cluster0-shard-00-02.2sqz0.mongodb.net:27017/?ssl=true&replicaSet=atlas-2839c0-shard-0&authSource=admin&appName=Cluster0";
 
 
 function main() {
